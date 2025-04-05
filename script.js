2fcc4d28ba4f4692284d88b2fdc46eb1cee591b7
hsassistec <hsassistec@gmail.com>
2025-04-05 04:32:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+  navigate('home'); // Ativa home ao carregar
+});
+
 function navigate(sectionId) {
   const sections = document.querySelectorAll('main section');
-  sections.forEach(section => section.classList.remove('active'));
-  document.getElementById(sectionId).classList.add('active');
+  sections.forEach(section => {
+    section.style.display = 'none';
+  });
+  const activeSection = document.getElementById(sectionId);
+  if (activeSection) {
+    activeSection.style.display = 'block';
+  }
 }
